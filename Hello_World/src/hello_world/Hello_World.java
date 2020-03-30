fae4ba427ee8d4dd46f37516e990fe834973e1ee
Joao7077 <56874568+Joao7077@users.noreply.github.com>
2020-03-30 14:02:59 -0400

    Add files via upload

---
+++ Hello_World/src/hello_world/Hello_World.java
@@ -0,0 +1,11 @@
+
+package hello_world;
+import javax.swing.JOptionPane;
+
+public class Hello_World {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello, world!");
+    }
+    
+}
